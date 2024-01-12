9f2c2e09cca431c48af40ebcd1632abf768f3b63
peterchoee <peter.choee@gmail.com>
2024-01-13 00:57:52 +0900

    Set android KSP & Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    alias(libs.plugins.jetbrainsKotlinKapt) apply false
+    alias(libs.plugins.googleDevtoolsKsp) apply false
     alias(libs.plugins.googleServices) apply false
     alias(libs.plugins.firebaseCrashlytics) apply false
     alias(libs.plugins.firebasePerf) apply false
-//    alias(libs.plugins.androidHilt) apply false
+    alias(libs.plugins.androidHilt) apply false
 }
