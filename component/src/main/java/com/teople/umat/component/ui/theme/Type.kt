fd50804bc89678f3623072c572cb3c83a8135e31
peterchoee <peter.choee@gmail.com>
2024-02-08 23:28:46 +0900

    update. custom theme (Colors & Typography)

--- component/src/main/java/com/teople/umat/component/ui/theme/Type.kt
+++ component/src/main/java/com/teople/umat/component/ui/theme/Type.kt
@@ -1,34 +1,125 @@
 package com.teople.umat.component.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.teople.umat.component.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+internal val lineSeedFamily = FontFamily(
+    Font(R.font.font_line_seed_bold, FontWeight.Bold),
+    Font(R.font.font_line_seed_regular, FontWeight.Normal)
 )
+
+internal val pretendardFamily = FontFamily(
+    Font(R.font.font_pretendard_bold, FontWeight.Bold),
+    Font(R.font.font_pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.font_pretendard_medium, FontWeight.Medium),
+    Font(R.font.font_pretendard_regular, FontWeight.Normal)
+)
+
+/**
+ * 디자인에서 제공 받은 한정된 폰트만 재구성해서 사용
+ */
+data class UmatTypography(
+    // LineSeed Bold
+    val lineSeedBold20: TextStyle = TextStyle(
+        fontFamily = lineSeedFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 30.sp,
+        fontSize = 20.sp
+    ),
+    val lineSeedBold18: TextStyle = TextStyle(
+        fontFamily = lineSeedFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 24.sp,
+        fontSize = 18.sp
+    ),
+    val lineSeedBold16: TextStyle = TextStyle(
+        fontFamily = lineSeedFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    val lineSeedBold12: TextStyle = TextStyle(
+        fontFamily = lineSeedFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    ),
+    // LineSeed Regular
+    val lineSeedRegular14: TextStyle = TextStyle(
+        fontFamily = lineSeedFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 24.sp,
+        fontSize = 14.sp
+    ),
+    // Pretendard Bold
+    val pretendardBold16: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    val pretendardBold12: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 14.sp,
+        fontSize = 12.sp
+    ),
+    // Pretendard SemiBold
+    val pretendardSemiBold18: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 24.sp,
+        fontSize = 18.sp
+    ),
+    val pretendardSemiBold16: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    val pretendardSemiBold14: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    val pretendardSemiBold12: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 14.sp,
+        fontSize = 12.sp
+    ),
+    // Pretendard Medium
+    val pretendardMedium16: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    val pretendardMedium12: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 14.sp,
+        fontSize = 12.sp
+    ),
+    // Pretendard Regular
+    val pretendardRegular16: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    val pretendardRegular12: TextStyle = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 14.sp,
+        fontSize = 12.sp
+    )
+)
+
+internal val LocalTypography = staticCompositionLocalOf { UmatTypography() }
