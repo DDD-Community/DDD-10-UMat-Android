dee44ad88658d985e6dc32a33f411f6330a62662
peterchoee <peter.choee@gmail.com>
2024-02-19 22:55:55 +0900

    add. search & navigator module

---
+++ feature/search/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.googleDevtoolsKsp)
+    alias(libs.plugins.androidHilt)
+}
+
+android {
+    namespace = "com.teople.umat.feature.search"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Module
+    implementation(project(":core:data"))
+    implementation(project(":component"))
+    implementation(project(":navigator"))
+
+    // Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+
+    implementation(libs.bundles.androidx)
+    implementation(libs.bundles.composeDependency)
+    implementation(libs.bundles.coil)
+
+    implementation(libs.bundles.coroutines)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+}
